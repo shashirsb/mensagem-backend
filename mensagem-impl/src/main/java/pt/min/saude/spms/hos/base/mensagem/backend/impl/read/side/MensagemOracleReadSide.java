a8315cc5639ef7bcd657dd55bb870f9ce08c4d88
shashi_bhushan_ramachandra <shashi.bhushan.ramachandra@oracle.com>
2021-04-06 00:08:00 +0530

    base oracle

--- mensagem-impl/src/main/java/pt/min/saude/spms/hos/base/mensagem/backend/impl/read/side/MensagemOracleReadSide.java
+++ mensagem-impl/src/main/java/pt/min/saude/spms/hos/base/mensagem/backend/impl/read/side/MensagemOracleReadSide.java
@@ -7,7 +7,7 @@ import io.vavr.concurrent.Future;
 import io.vavr.control.Option;
 import pt.min.saude.spms.hos.base.mensagem.backend.impl.schema.MensagemState;
 import pt.min.saude.spms.hos.common.classes.backend.LogBuilder;
-import pt.min.saude.spms.hos.oracle.utils.backend.BaseOracleaReadSide;
+import pt.min.saude.spms.hos.oracle.utils.backendBaseOracleaReadSide;
 
 
 import javax.inject.Inject;
