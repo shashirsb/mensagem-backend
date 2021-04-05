826e73bc552e8776b275f7f5137d95b52aae384a
shashi_bhushan_ramachandra <shashi.bhushan.ramachandra@oracle.com>
2021-04-06 00:01:03 +0530

    adding

--- mensagem-impl/src/main/java/pt/min/saude/spms/hos/base/mensagem/backend/impl/read/side/BaseOracleReadSide.java
+++ mensagem-impl/src/main/java/pt/min/saude/spms/hos/base/mensagem/backend/impl/read/side/BaseOracleReadSide.java
@@ -3,7 +3,7 @@
  package pt.min.saude.spms.hos.base.mensagem.backend.impl.read.side;
  public abstract class BaseOracleReadSide<T> implements pt.min.saude.spms.hos.common.classes.backend.Upgradable {
    
-   protected final pt.min.saude.spms.hos.common.classes.backend.LogBuilder log=null;
+   protected final pt.min.saude.spms.hos.common.classes.backend.LogBuilder log=;
    
    protected final com.lightbend.lagom.javadsl.persistence.jpa.JpaSession jpaSession;
    protected final java.lang.String oracleDatabase;
@@ -16,8 +16,7 @@
    
    protected com.datastax.driver.core.PreparedStatement upsertStatement;
    
-   public  BaseOracleReadSide(pt.min.saude.spms.hos.common.classes.backend.LogBuilder log, com.typesafe.config.Config configuration, com.lightbend.lagom.javadsl.persistence.jpa.JpaSession jpaSession, io.vavr.control.Option<java.lang.String> configKeyOracleKeyspace, akka.actor.ActorSystem actorSystem, java.lang.Class<T> stateClass) {
-   }
+   public  BaseOracleReadSide(pt.min.saude.spms.hos.common.classes.backend.LogBuilder log, com.typesafe.config.Config configuration, com.lightbend.lagom.javadsl.persistence.jpa.JpaSession jpaSession, io.vavr.control.Option<java.lang.String> configKeyOracleKeyspace, akka.actor.ActorSystem actorSystem, java.lang.Class<T> stateClass) {}
    
   //  public  BaseOracleReadSide(pt.min.saude.spms.hos.common.classes.backend.LogBuilder log, com.typesafe.config.Config configuration, com.lightbend.lagom.javadsl.persistence.jpa.JpaSession jpaSession, io.vavr.control.Option<java.lang.String> configKeyOracleKeyspace, io.vavr.control.Option<java.lang.String> tableName, akka.actor.ActorSystem actorSystem, java.lang.Class<T> stateClass) {
   //  }
